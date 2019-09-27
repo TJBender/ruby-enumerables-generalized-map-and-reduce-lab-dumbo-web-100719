7a571947643e940feb55d625712442ffeac81b54
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 00:37:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+def map(array, block)
+  new_array = []
+  i = 0
+  while i < array.length
+  map(array){block}
+  end
+end
+
+def reduce(array, starting_val = 0)
+  i = 0
+  while i < array.length
+    if true
+      starting_val += 1
+    end
+  i+= 1
+  end
+starting_val
+end
